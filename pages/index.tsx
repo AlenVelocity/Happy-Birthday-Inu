a291a4d1864f464692a882bcce8705c0dd062aa7
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2022-03-23 21:07:12 +0530

    feat(): add audio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
 import Card from '../components/Card'
 import Header from '../components/Header'
 
 const Home: NextPage<{ cards: Array<{ author: string, text: string }>}> = (props)  => {
-    console.log(props)
     return (
         <div className="container mx-auto px-4 py-16 bg-gradient-to-r from-red-300 to-pink-400">
             <Head>
@@ -17,6 +17,7 @@ const Home: NextPage<{ cards: Array<{ author: string, text: string }>}> = (props
             <Header></Header>
 
             <main className="flex flex-col items-center col-span-2">
+                <audio src="/precious_photographs.mp3" autoPlay loop></audio>
             <div className='flex flex-col items-center col-span-2'>
                 {props.cards.map((card, index) => <Card author={card.author} text={card.text} i={index} key={index}/>)}
             </div>
